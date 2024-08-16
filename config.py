79baf3488d8deac267eef127ab0d065a86a22538
Gabriel Sandoval <116860355+gabsthedeveloper@users.noreply.github.com>
2024-08-16 06:53:47 -0700

    Create config.py
    Adding sample config.py file

---
+++ config.py
@@ -0,0 +1,10 @@
+driver_path = '/your/path/chromedriver'
+suspect = dict(
+    first_name = 'first',
+    last_name = 'last',
+    gender = 'gender',
+    city = 'city',
+    approximate_birth_year = None #suspect's approximate birth year if known, else none
+)
+fraud_reason = 'This is where you would describe the fraud in details'
+additional_information = 'This is where you would write additional information that might be helpful for the report'
